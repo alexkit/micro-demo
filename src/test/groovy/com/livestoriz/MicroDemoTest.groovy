f77ca3811d7d1eb832434a933ebd4eed098bc6b0
Alex Kovacs <alexkit>
2020-12-12 08:43:27 +0000

    Initial commit

---
+++ src/test/groovy/com/livestoriz/MicroDemoTest.groovy
@@ -0,0 +1,19 @@
+package com.livestoriz
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+
+@MicronautTest
+class MicroDemoTest {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    @Test
+    void testItWorks() {
+        assert application.running == true
+    }
+
+}
